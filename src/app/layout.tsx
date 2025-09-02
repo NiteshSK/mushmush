c0d64c32798bd4e93847986ca10a33c830f71ec3
Nitesh Singh Kataria <kataria@Niteshs-MacBook-Air.local>
2025-09-02 20:57:58 +0530

    fix the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="font-euclid-circular-a">
+      <body>
         <ClientRootLayout>
           {children}
         </ClientRootLayout>
